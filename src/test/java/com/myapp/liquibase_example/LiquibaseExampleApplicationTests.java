e7ab0fc7e006d510df6fb128d6ae996fb6d41a1a
Nuwan <nuwan199044@gmail.com>
2025-05-17 14:21:52 +0530

    initial code

---
+++ src/test/java/com/myapp/liquibase_example/LiquibaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.liquibase_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
